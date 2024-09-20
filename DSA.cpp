53059af37a73e04c3d744e0c9b880ba499727af5
Khalid Abbas <159667330+carlitobarcha@users.noreply.github.com>
2024-09-20 20:38:41 +0500

    Add files via upload

---
+++ DSA.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number = 10;        
+          
+    float decimal = 5.5;    
+
+    
+    cout << "Number: " << number << endl;
+    
+    cout << "Decimal: " << decimal << endl;
+
+    return 0;
+}
+
